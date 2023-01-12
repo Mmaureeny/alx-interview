a0b9f3653786c865ccf9010571e05fac090f04a2
Mmaureeny <muthonimaureen4@outlook.com>
2023-01-12 10:47:48 -0800

    operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,8 +1,11 @@
-"""
+'''
  method that calculates the fewest number of operations needed to result in exactly n H characters in the file.
-"""
+'''
 
 def minOperations(n):
+    '''Computes the fewest number of operations needed to result
+    in exactly n H characters.
+    '''
     if n <= 0:
         return 0
     operations = 0
